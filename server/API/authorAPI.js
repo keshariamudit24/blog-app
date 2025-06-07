ed533650fd0e12d4e3e0a881f5c4d92ecbced0f2
keshariamudit24 <keshariamudit@gmail.com>
2025-06-07 17:26:04 +0530

    read articles route handler implemented

--- server/API/authorAPI.js
+++ server/API/authorAPI.js
@@ -2,13 +2,23 @@ const express = require("express")
 const authorApp = express.Router();
 const expressAsyncHandler = require("express-async-handler");
 const createUserOrAuthor = require("./createUserOrAuthor")
+const Article = require("../models/articleModel")
 
 // create new author 
 authorApp.post("/author", expressAsyncHandler(createUserOrAuthor))
+
 // create an article 
 authorApp.post("/article", expressAsyncHandler(async (req, res) => {
-    const authorArticle = req.body
-    console.log(authorArticle)
+    const newArticleObject = req.body
+    const newArticle = new Article(newArticleObject);
+    const savedArticle = await newArticle.save();
+    res.status(201).send({ msg: "Article created", payload: savedArticle });
+}))
+
+// read article
+authorApp.get("/articles", expressAsyncHandler(async (req, res) => {
+    const listOfArticles = await Article.find();
+    res.status(200).send({ msg: "articles", payload: listOfArticles });
 }))
 
 // export 
