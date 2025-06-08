ab8cb0f5baa377323aa2418cfefbe7cc7b2beadd
keshariamudit24 <keshariamudit@gmail.com>
2025-06-08 17:30:02 +0530

    implemented routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,83 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './components/RootLayout.jsx';
+import Home from './components/common/Home.jsx';
+import Header from './components/common/Header.jsx';
+import Footer from './components/common/Footer.jsx';
+import Articles from './components/common/Articles.jsx';
+import ArticleById from './components/common/ArticleById.jsx';
+import Signin from './components/common/Signin.jsx';
+import Signup from './components/common/Signup.jsx';
+import UserProfile from './components/user/userProfile.jsx';
+import AuthorProfile from './components/author/AuthorProfile.jsx';
+import PostArticle from './components/author/postArticle.jsx';
+
+// array of objects 
+const browserRouterObj = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "signin",
+        element: <Signin />
+      },
+      {
+        path: "signup",
+        element: <Signup />
+      },
+      {
+        path: "user-profile/:email",
+        element: <UserProfile />,
+        children: [
+          {
+            path: "articles",
+            element: <Articles />
+          },
+          {
+            path: ":articleId",
+            element: <ArticleById />
+          },
+          {
+            path: "",
+            element: <Navigate to="articles" />
+          }
+        ]
+      },
+      {
+        path: "author-profile/:email",
+        element: <AuthorProfile />,
+        children: [
+          {
+            path: "articles",
+            element: <Articles />
+          },
+          {
+            path: ":articleId",
+            element: <ArticleById />
+          },
+          {
+            path: "article",
+            element: <PostArticle />
+          },
+          {
+            path: "",
+            element: <Navigate to="articles" />
+          }
+        ]
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={browserRouterObj} />
   </StrictMode>,
 )
